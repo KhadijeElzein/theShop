5131eb6411cc956291e68f794fd08f101a297848
ZEIN <khadije.el@atos.net>
2019-12-17 09:05:44 -0300

    created functionality to see the details of a product

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from core import views
 
 urlpatterns = [
     path('', views.list_products, name='product_list'),
+    path('<int:pk>', views.detail_product, name='product_detail'),
 ] 
--- core/views.py
+++ core/views.py
@@ -4,3 +4,10 @@ from .models import Product
 def list_products(request):
     products = Product.objects.all() 
     return render(request,'index.html',{'products': products})
+
+
+def detail_product(request,pk):
+    product = Product.objects.get(pk=pk) 
+    return render(request,'detail.html',{'product': product})
+
+
